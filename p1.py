ddbd305ddda94543c6a59390dc9fc3e13de265eb
Abhigyan Pal <palabhigyan715@gmail.com>
2024-09-18 16:41:12 +0530

    PANN1

---
+++ p1.py
@@ -0,0 +1,6 @@
+# lecture 1
+input = [1.2,5.1,2.1] # input from previous neurons
+weights = [3.1,2.1,8.7]
+bias = 3 # for current neuron
+output = input[0]*weights[0] + input[1]*weights[1] + input[2]*weights[2] + bias
+print(output)
